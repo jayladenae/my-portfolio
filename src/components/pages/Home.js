0b83e906064953f356bb3e6993cd8f1003a2ec88
jayladenae <jaydenaen@icloud.com>
2021-11-24 00:52:28 -0600

    navbar done and about me page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,9 +2,10 @@ import React from 'react';
 import "../styles/Home.css";
 export default function Home() {
     return (
-      <div id="container-home">
-      <h1 >About Me</h1>
-      <p >Smallest directly families surprise honoured am an. Speaking replying mistress him numerous she returned
+      <div id="container-home1">
+      <div id="container-home2">
+      <h1 id="about-me" >About Me</h1>
+      <p id="about-me-p">Smallest directly families surprise honoured am an. Speaking replying mistress him numerous she returned
                    feelings may day. Evening way luckily son exposed get general greatly. Zealously prevailed be arranging
                    do.
                    Set arranging too dejection september happiness. Understood instrument or do connection no appearance do
@@ -21,17 +22,30 @@ export default function Home() {
                    Enjoyment
                    discourse ye continued pronounce we necessary abilities.
                </p>
-               <h1 >Skills:</h1>
-           <div  >
-           <i  className="fab fa-html5 fa-4x"></i>
-           <i  className="fab fa-react fa-4x"></i>
-           <i  className="fab fa-css3-alt fa-4x"></i>
-           <i  className="fab fa-bootstrap fa-4x"></i>
-           <i  className="fab fa-mdb fa-4x"></i>
-           <i  className="fab fa-js-square fa-4x"></i>
-           <i  className="fab fa-node fa-4x"></i>
-           <i  className="fab fa-figma fa-4x"></i>
+               <br></br>
+                   <br></br>
+           <h1 id="lang-skill" >languages & skills:</h1>
+           <div id="skills" >
+           <a href="https://babeljs.io/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/babeljs/babeljs-icon.svg" className="icon-skill" alt="babel" width="50" height="50"/> </a> 
+           <a href="https://getbootstrap.com" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg" className="icon-skill" alt="bootstrap" width="50" height="50"/> </a>
+            <a href="https://www.w3schools.com/css/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" className="icon-skill" alt="css3" width="50" height="50"/> </a> 
+            <a href="https://expressjs.com" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg" className="icon-skill" alt="express" width="50" height="50"/> </a> 
+            <a href="https://www.figma.com/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/figma/figma-icon.svg" className="icon-skill" alt="figma" width="50" height="50"/> </a> 
+            <a href="https://heroku.com" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/heroku/heroku-icon.svg" className="icon-skill" alt="heroku" width="50" height="50"/> </a> 
+            <a href="https://www.w3.org/html/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" className="icon-skill" alt="html5" width="50" height="50"/> </a>
+            
+            <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" className="icon-skill" alt="javascript" width="50" height="50"/> </a>
+           
+            <a href="https://jestjs.io" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/jestjsio/jestjsio-icon.svg" className="icon-skill" alt="jest" width="50" height="50"/> </a> 
+            <a href="https://materializecss.com/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/prplx/svg-logos/5585531d45d294869c4eaab4d7cf2e9c167710a9/svg/materialize.svg" className="icon-skill" alt="materialize" width="50" height="50"/> </a>
+            <a href="https://www.mongodb.com/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg" className="icon-skill" alt="mongodb" width="50" height="50"/> </a>
+            <a href="https://www.mysql.com/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg" className="icon-skill" alt="mysql" width="50" height="50"/> </a> 
+            <a href="https://nodejs.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" className="icon-skill" alt="nodejs" width="50" height="50"/> </a>
+            <a href="https://reactjs.org/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" className="icon-skill" alt="react" width="50" height="50"/> </a>
+            <a href="https://tailwindcss.com/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg" alt="tailwind" width="50" className="icon-skill" height="50"/> </a>
+            <a href="https://webpack.js.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/d00d0969292a6569d45b06d3f350f463a0107b0d/icons/webpack/webpack-original-wordmark.svg" className="icon-skill" alt="webpack" width="50" height="50"/> </a>
            </div>
+      </div>
       </div>
     );
   }
